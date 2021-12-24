4f82b6d58fadc138dcdd2f3c2caac128edf708b8
Mohammad Azam <azamsharp@gmail.com>
2021-12-24 11:59:11 -0600

    Initial Commit

---
+++ Maps/MapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapsApp.swift
+//  Maps
+//
+//  Created by Mohammad Azam on 12/24/21.
+//
+
+import SwiftUI
+
+@main
+struct MapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
